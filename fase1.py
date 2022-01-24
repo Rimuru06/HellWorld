a6458aa8d9c94820351edca221e92798dc94b3c8
Thiago Aragão <thiago.alpha.06@gmail.com>
2022-01-24 17:51:39 -0300

    depois vou continuar

--- fase1.py
+++ fase1.py
@@ -20,7 +20,7 @@ def fase1(janela, teclado, modulo, nivelDificuldade):
      timerJump = 0
      velJump = 400
      velPlayer = 200
-     velTiro = 800
+     velTiro = 500
      jump = False
      changeDJump = False
      alturaJump = player.y - 2.5*player.height
@@ -30,6 +30,10 @@ def fase1(janela, teclado, modulo, nivelDificuldade):
      playerVoltadoPraFrente = True
      timerTiro = 0
      playerAtirou = False
+     etapaFase = 0.5
+     vidasPlayer = 10
+     zumbi = Sprite("zumbiEsquerda.png", 1)
+     listaZumbis = []
 
      while modulo == 2:
           tempoTrocaSprite += janela.delta_time()
@@ -108,36 +112,61 @@ def fase1(janela, teclado, modulo, nivelDificuldade):
                playerAtirou = True
                if playerVoltadoPraFrente == True:
                     tiroPlayer = Sprite("tiroPlayer.png", 1)
-                    tiroPlayer.y = player.y + 10
+                    tiroPlayer.y = player.y + 30
                     tiroPlayer.x = player.x + player.width
                     projeteisPlayerFrente.append(tiroPlayer)
-                    print("DEVIA ESTAR ATIRANDO PRA FRENTE")
                elif playerVoltadoPraFrente == False:
                     tiroPlayer = Sprite("tiroPlayer.png", 1)
-                    tiroPlayer.y = player.y + 10
+                    tiroPlayer.y = player.y + 30
                     tiroPlayer.x = player.x
                     projeteisPlayerTras.append(tiroPlayer)
-                    print("DEVIA ESTAR ATIRANDO PRA TRÁS")
           
           if playerAtirou == True:
                timerTiro += janela.delta_time()
                if timerTiro > 0.2:
                     playerAtirou = False
                     timerTiro = 0
-          
-          for t in range(len(projeteisPlayerFrente)-1, -1, -1):
-               projeteisPlayerFrente[t].x += velTiro*janela.delta_time()
-               projeteisPlayerFrente[t].draw()
-          
-          for t in range(len(projeteisPlayerTras)-1, -1, -1):
-               projeteisPlayerTras[t].x -= velTiro*janela.delta_time()
-               projeteisPlayerTras[t].draw()
 
-          cenario.draw()
-          chao.draw()
+          if etapaFase == 1:
+               cenario.draw()
+               chao.draw()
+               for t in range(len(projeteisPlayerFrente)-1, -1, -1):
+                    projeteisPlayerFrente[t].draw()
+                    projeteisPlayerFrente[t].x += velTiro*janela.delta_time()
+                    if projeteisPlayerFrente[t] > janela.width:
+                         projeteisPlayerFrente.remove(projeteisPlayerFrente[t])
+               
+               for t in range(len(projeteisPlayerTras)-1, -1, -1):
+                    projeteisPlayerTras[t].draw()
+                    projeteisPlayerTras[t].x -= velTiro*janela.delta_time()
+                    if projeteisPlayerTras[t] < 0:
+                         projeteisPlayerTras.remove(projeteisPlayerTras[t])
+               if player.x > janela.width:
+                    etapaFase = 1.5
+          
+          if etapaFase == 1.5:
+               zumbix = janela.width - 40
+               zumbiAndandoEsquerda = True
+               for l in range(0, 4, +1):
+                    zumbi = Sprite("zumbiEsquerda.png", 1)
+                    zumbi.y = chao.y - zumbi.height
+                    zumbi.x = zumbix
+                    listaZumbis.append(zumbi)
+                    zumbix -= 100
+               etapaFase = 2
+          
+          if etapaFase == 2:
+               for l in range(len(listaZumbis)-1, -1, -1):
+                    
+                    listaZumbis[l].draw()
+          
           player.draw()
+          janela.draw_text("Vidas: " + str(vidasPlayer), 10, janela.height - 70, 30, (255, 0, 0), "Arial", True, False)
           player.update()
           janela.update()
           fps.tick(60)
+
+          if vidasPlayer <= 0:
+               modulo = 1
      
      return modulo
